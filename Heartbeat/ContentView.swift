2c562271a52d4b02c27ff7aa9a2e2e3024570b9b
Yaroslav Malygin <ya.malygin@team.superjob.ru>
2025-02-15 17:28:36 +0300

    Initial Commit

---
+++ Heartbeat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Heartbeat
+//
+//  Created by Yaroslav Malygin on 15.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Heartbeat/HeartbeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartbeatApp.swift
+//  Heartbeat
+//
+//  Created by Yaroslav Malygin on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct HeartbeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
